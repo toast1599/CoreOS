5170a4f145d96340a7d9dd91fe4087399610dfb1
toast1599 <swizzguy76@gmail.com>
2026-03-15 18:59:01 -0400

    debugging macro IMPROVED because of single serial write, file and line numbers, and more

--- kernel/src/debug.rs
+++ kernel/src/debug.rs
@@ -1,11 +1,25 @@
-/// Verbose debug logging to COM1 serial.
-/// Usage:  dbg_log!("TAG", "message {}", value);
+//! Simple kernel debug logging for COM1 serial.
+//!
+//! Usage:
+//!     dbg_log!("TASK", "spawned task {}", id);
+
+/// Compile-time switch to enable/disable debug logs.
+pub const DEBUG_ENABLED: bool = true;
+
+/// Internal logging macro.
+/// Do not call directly outside the crate.
 #[macro_export]
 macro_rules! dbg_log {
     ($tag:expr, $($arg:tt)*) => {{
-        $crate::serial_fmt!("[{}] ", $tag);
-        $crate::serial_fmt!($($arg)*);
-        $crate::serial_fmt!("\n");
+        if $crate::debug::DEBUG_ENABLED {
+            $crate::serial_fmt!(
+                "[{}][{}:{}] {}\n",
+                $tag,
+                core::file!(),
+                core::line!(),
+                format_args!($($arg)*)
+            );
+        }
     }};
 }
 
